0df4a2530ac2eebe00a2f20aa4e1a8021950fa5b
JuRyeon33 <thwnfus0330@gmail.com>
2025-07-21 16:27:32 +0900

    Initial commit

---
+++ src/main/java/com/julyun123/inventory_app/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.julyun123.inventory_app.repository;
+
+import com.julyun123.inventory_app.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, QuerydslPredicateExecutor<Product> {
+}
